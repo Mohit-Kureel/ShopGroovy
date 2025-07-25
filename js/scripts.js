18f0781f301bfdd88ccf3c129f4c37272e504adf
Mohit-Kureel <mohitkureel@gmail.com>
2025-07-25 23:51:03 +0530

    vn footer reduced

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const alertButton = document.getElementById('alertButton');
 
     alertButton.addEventListener('click', function() {
-        alert('I love you ❤️');
+        alert('Goodnight baby, Take care 😘❤️');
     });
 });
