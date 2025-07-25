48823765ed0b77336966b936472b172821e2c0e6
Mohit-Kureel <mohitkureel@gmail.com>
2025-07-25 23:08:19 +0530

    vn2

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const alertButton = document.getElementById('alertButton');
 
     alertButton.addEventListener('click', function() {
-        alert('Yay! You clicked the button. You are ready to host this site!');
+        alert('I love you ❤️');
     });
 });
